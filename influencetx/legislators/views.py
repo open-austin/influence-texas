acbbb2a01856640265f4ca3af6e6bf0ad2dee257
Tony S Yu <tsyu80@gmail.com>
2017-07-17 00:37:24 -0500

    Improve functionality of legislator-detail page

--- influencetx/legislators/views.py
+++ influencetx/legislators/views.py
@@ -14,4 +14,20 @@ class LegislatorDetailView(DetailView):
 
     model = models.Legislator
     context_object_name = 'legislator'
-    queryset = models.Legislator.objects.all().prefetch_related('votes__vote_tally__bill')
+    queryset = (
+        models.Legislator.objects.all()
+            .prefetch_related('votes__vote_tally__bill__subjects')
+    )
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(LegislatorDetailView, self).get_context_data(*args, **kwargs)
+
+        votes = []
+        for each in self.object.votes.all():
+            tally = each.vote_tally
+            bill = tally.bill
+            subjects = [subject.label for subject in bill.subjects.all()]
+            votes.append({'value': each.value, 'date': tally.date,
+                          'bill': tally.bill, 'subjects': subjects})
+        context['votes'] = votes
+        return context
