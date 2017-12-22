ffcd41acbc06a0f4fd04f2a549d17536b2e5ccc7
Tony S Yu <tsyu80@gmail.com>
2017-12-21 19:41:53 -0600

    Add admin for bills app

---
+++ influencetx/bills/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.Bill)
+admin.site.register(models.SubjectTag)
+admin.site.register(models.ActionDates)
+admin.site.register(models.VoteTally)
+admin.site.register(models.SingleVote)
