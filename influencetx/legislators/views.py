8cd3e051566f1f875803d92d77bf89fad0315fc8
Tony S Yu <tsyu80@gmail.com>
2017-12-25 12:10:31 -0800

    Add Senator and Representative list-views

--- influencetx/legislators/views.py
+++ influencetx/legislators/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import DetailView, ListView
 
+from influencetx.core import constants
 from . import models
 
 
@@ -7,10 +8,32 @@ class LegislatorListView(ListView):
 
     model = models.Legislator
     context_object_name = 'legislators'
-    queryset = (
-        models.Legislator.objects.all()
-        .order_by('first_name', 'last_name')
-    )
+    extra_context = {'title': 'All Legislators'}
+    filters = {}
+
+    def get_queryset(self, *args, **kwargs):
+        return (
+            models.Legislator.objects
+            .filter(**self.filters)
+            .order_by('first_name', 'last_name')
+        )
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(LegislatorListView, self).get_context_data(*args, **kwargs)
+        context.update(**self.extra_context)
+        return context
+
+
+class SenatorListView(LegislatorListView):
+
+    extra_context = {'title': 'Senators'}
+    filters = {'chamber': constants.Chamber.UPPER.value}
+
+
+class RepresentativeListView(LegislatorListView):
+
+    extra_context = {'title': 'Representatives'}
+    filters = {'chamber': constants.Chamber.LOWER.value}
 
 
 class LegislatorDetailView(DetailView):
