1a240a0c5a4b589c2711e1da010a62bb85905017
Niicck <niicck@users.noreply.github.com>
2019-06-02 16:38:44 -0500

    fix database connection, automate data loading

--- src/entrypoint.nick.sh
+++ src/entrypoint.nick.sh
@@ -4,12 +4,16 @@ set -o errexit
 # Start outputting logs to stdout
 tail -n 0 -f ../logs/gunicorn*.log &
 
-# Only use raven for production environments?
-# export DJANGO_SETTINGS_MODULE=config.settings.production
+# Loop sleep every second until a db connection is available.
+until psql $DATABASE_URL -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping ..."
+  sleep 1
+done
+>&2 echo "Postgres is up - executing command"
 
 # Run migrations
 python3 manage.py migrate
 
-echo "Done"
+echo "Migrations done. Running at http://localhost:5120"
 
 exec "$@"
--- src/run-local.sh
+++ src/run-local.sh
@@ -3,16 +3,11 @@ set -o errexit
 DIR=`dirname $BASH_SOURCE`
 
 # Build a docker image using your local configs
-docker build --build-arg APP_ENV=nick -f $DIR/Dockerfile.nick -t inftxos:local $DIR
+DOCKER_BUILDKIT=1 docker build --build-arg APP_ENV=nick -f $DIR/Dockerfile.nick -t inftxos:local $DIR
 
 # Source your env variables for use in docker-compose
 source $DIR/env.sh
+export COMPOSE_PROJECT_NAME=inftxos
 
 # Run it
 docker-compose -f $DIR/docker-compose.nick up
-
-# exec "$@" isn't working
-# ideally
-# docker run -it inftxos:local bin/bash
-# would be sufficient
-# docker run -it --entrypoint /bin/bash inftxos:local
