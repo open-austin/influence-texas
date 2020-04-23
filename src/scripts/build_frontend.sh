fd2e7fd9953419e0c1fa24d1dfa9881037e7179f
Michael Rollins <lazarus1331@gmail.com>
2020-04-22 21:28:38 -0500

    112: fix npm build process and docker-compose for vagrant and build

--- src/scripts/build_frontend.sh
+++ src/scripts/build_frontend.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 DIR=`dirname $BASH_SOURCE`
-# this will build the frontend assets inside a docker contianer 
-docker rm itreactrun || true 
+# this will build the frontend assets inside a docker contianer
+docker rm itreactrun || true
 docker build -t itreact $DIR/../frontend
 # `npm install` and `npm run build` both take a minute
 docker run --ipc=host --name=itreactrun itreact 
 # copy the newly built files to the host
 docker cp itreactrun:/usr/src/influencetx/static/react-app $DIR/../influencetx/static
 # and open so you can see it worked
-open  $DIR/../influencetx/static/react-app/index.html
+#open  $DIR/../influencetx/static/react-app/index.html
