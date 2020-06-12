cc78cc0b3cd7ebe4ffb89586bbd575f648d715c3
Tracy Mullen <tracykmullen@gmail.com>
2020-06-12 10:18:09 -0500

    subscription test

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,29 +1,86 @@
 import React from 'react'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { BrowserRouter as Router } from 'react-router-dom'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-client'
+import { ApolloProvider, useSubscription, useQuery } from '@apollo/react-hooks'
 import { legTheme } from './theme'
 import ScrollToTop from './ScrollToTop'
 import Routes from './Routes'
+import { split } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import { gql } from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+// Create an http link:
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3003/graphql',
+})
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:3003/subscriptions`,
+  options: {
+    reconnect: true,
+  },
+})
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink,
+)
 
 const client = new ApolloClient({
-  uri:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:5120/graphql/'
-      : '/graphql/',
+  link,
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:3003/graphql',
 })
+
+const BOOK_SUBSCRIPTION = gql`
+  subscription bookAdded($title: String!) {
+    bookAdded(title: $title) {
+      title
+      author
+    }
+  }
+`
+const BOOKS_QUERY = gql`
+  query {
+    books {
+      title
+      author
+    }
+  }
+`
+
+function BookList() {
+  const { data, loading } = useSubscription(BOOK_SUBSCRIPTION, {
+    variables: { title: 'boo' },
+  })
+  const q = useQuery(BOOKS_QUERY)
+
+  return (
+    <div>
+      {q.data && q.data.books.map((book) => <div>{book.title}</div>)}
+      {data && <h4>Just added: {data.bookAdded.title}</h4>}
+    </div>
+  )
+}
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router basename="v2">
-        <MuiThemeProvider theme={legTheme}>
-          <ScrollToTop>
-            <Routes />
-          </ScrollToTop>
-        </MuiThemeProvider>
-      </Router>
+      <BookList />
     </ApolloProvider>
   )
 }
